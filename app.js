4dd860d8df1f1d1480b8acc892ed729d876579bc
ValentinaZhinko <valentina.zhinko97@gmail.com>
2021-02-11 20:54:56 +0300

    Task3

---
+++ app.js
@@ -0,0 +1,13 @@
+let a = 1;
+let b ='hello';
+let c = null;
+let d = true;
+let e;
+let f ={name:'Vasya'};
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+
